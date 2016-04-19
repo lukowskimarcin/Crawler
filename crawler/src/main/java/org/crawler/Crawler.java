11841b257c0931fb47989627f1167e3f32b23a6f
Marcin <Marcin@pclukowski>
2016-04-19 16:27:14 +0200

    asd

--- crawler/src/main/java/org/crawler/Crawler.java
+++ crawler/src/main/java/org/crawler/Crawler.java
@@ -8,13 +8,20 @@ package org.crawler;
 public abstract class Crawler<TPage extends Page<?>> implements ICrawler{
 	private Manager manager;
 	private TPage currentPage;
-
+	private CrawlerStatus status;
 	
 	public Crawler(Manager manager) {
 		this.manager = manager;
+		status = CrawlerStatus.STOPED;
 	}
 	
 	public abstract boolean canParsePage(Page<?> page);
 	
+	
+	
+	
+	public TPage getCurrentPage() {
+		return currentPage;
+	}
 	
 }
---
+++ crawler/src/main/java/org/crawler/CrawlerStatus.java
@@ -0,0 +1,5 @@
+package org.crawler;
+
+public enum CrawlerStatus {
+   WAITING_FOR_PAGE, PROCESSING_PAGE, STOPED, PAUSED;
+}
